66f6afc492aa0c17fccf8b4f5241e132f018c196
Pinny Geliebter <pgibralter@gmail.com>
2021-09-10 18:17:13 -0400

    adds knack API test call

--- api-test.rb
+++ api-test.rb
@@ -1,2 +1,11 @@
 require "dotenv/load"
-p ENV["S3_BUCKET"]
+require "http"
+
+num = 1
+
+5.times do
+  response = HTTP.headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"])
+    .get("https://usgc-api.knack.com/v1/objects/object_#{num}/records")
+  num += 1
+  p response.parse
+end
