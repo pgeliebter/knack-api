ed6dc2afa35a13c27c0f4a13ac0f35d6582fb338
Pinny Geliebter <pgibralter@gmail.com>
2021-09-11 22:37:11 -0400

    adds pg gem

--- api-test.rb
+++ api-test.rb
@@ -1,11 +1,21 @@
 require "dotenv/load"
 require "http"
+require "pg"
 
 num = 1
 
-5.times do
-  response = HTTP.headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"])
-    .get("https://usgc-api.knack.com/v1/objects/object_#{num}/records")
-  num += 1
-  p response.parse
+# 5.times do
+#   response = HTTP.headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"])
+#     .get("https://usgc-api.knack.com/v1/objects/object_#{num}/records")
+#   num += 1
+#   p response.parse
+# end
+
+conn = PG.connect(dbname: "movie_app_development")
+conn.exec("SELECT * FROM pg_stat_activity") do |result|
+  puts "     PID | User             | Query"
+  result.each do |row|
+    puts " %7d | %-16s | %s " %
+           row.values_at("pid", "usename", "query")
+  end
 end
