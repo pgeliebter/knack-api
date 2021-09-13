bf3b229984404f2d01ca5bc6df91a5c47b43fd02
Pinny Geliebter <pgibralter@gmail.com>
2021-09-13 10:53:29 -0400

    testing getting the knack_id

--- api-test.rb
+++ api-test.rb
@@ -20,12 +20,15 @@ require "addressable/uri"
 #   p response.parse
 # end
 
+# # the below was just a test of a working put loop
 # @problem_codes.each do |_match_field, id|
+#   p [_match_field, id]
 #   response = HTTP
 #     .headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"], "content-type" => "application/json")
-#     .put("https://usgc-api.knack.com/v1/objects/#{@problem_code_object}/records/#{id}", :json => { :field_1226 => id })
+#   # MUST CHANGE FIELD WHEN IN LIVE APP
+#     .put("https://usgc-api.knack.com/v1/objects/#{@problem_code_object}/records/#{id}", :json => { "field_1225": id, "field_1226": "now", "field_1227": DateTime.now.strftime("%m/%d/%Y %I:%M%P") })
 #   p response.parse(:json)
-#   if response.code == 400
+#   if response.code != 200
 #     pp response
 #     break
 #   end
@@ -51,12 +54,32 @@ require "addressable/uri"
 #   end
 # end
 
-# this is a get request that returns a single record
-filters = [{ "field" => "field_197", "operator" => "is", "value" => "2" }]
-filters = JSON.generate(filters).to_s
-filters = Addressable::URI.encode_component(filters)
-response = HTTP
-  .headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"])
-  .get("https://usgc-api.knack.com/v1/objects/object_6/records?filters=#{filters}")
-body = JSON.parse(response.body)
-p session_id = body["records"][0]["id"]
+# CSV stuff
+# static headers for new csv file
+headers = ["ID", "Mandate Id", "Accounting Id", "Match Field", "Paid On", "Paid", "Import Code", "Knack Id"]
+# Open up the new csv file
+CSV.open("TurnaroundImport_Test_Write.csv", "w") do |csv|
+  # import headers into new file
+  csv << headers
+  # open up original csv line by line
+  CSV.foreach("TurnaroundImport_9-7-21_Test.csv", headers: true, header_converters: :symbol) do |row|
+    row["Import Code"] = @problem_codes[row[:match_field]]
+    # this is a get request that returns a single record
+    filters = [{ "field" => "field_197", "operator" => "is", "value" => row[:id] }]
+    filters = JSON.generate(filters).to_s
+    filters = Addressable::URI.encode_component(filters)
+    response = HTTP
+      .headers("X-Knack-Application-Id" => ENV["KNACK_APP_ID"], "X-Knack-REST-API-KEY" => ENV["KNACK_API_KEY"])
+      .get("https://usgc-api.knack.com/v1/objects/object_6/records?filters=#{filters}")
+    body = JSON.parse(response.body)
+    p session_id = body["records"][0]["id"]
+    p response.code
+    row[:knack_id] = session_id
+
+    csv << row
+    if row[:paid_on]
+      parsed_date = Date.strptime(row[:paid_on], "%m/%d/%y")
+      row[:paid_on] = parsed_date
+    end
+  end
+end
--- problem_code.rb
+++ problem_code.rb
@@ -1,4 +1,4 @@
-@problem_codes = JSON.parse(File.read("../../Documents/sessionproblemimportcodes.json"))
+@problem_codes = JSON.parse(File.read("../../Documents/sessionproblemimportcodes copy.json"))
 @problem_codes = @problem_codes["records"].map { |x|
   [x["field_605"], x["id"]]
 }.to_h
