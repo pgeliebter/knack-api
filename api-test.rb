3839194304398b3338206aaa16165cc8d5e314a1
Pinny Geliebter <pgibralter@gmail.com>
2021-09-12 20:55:44 -0400

    adds CSV reading and writing

--- api-test.rb
+++ api-test.rb
@@ -21,3 +21,23 @@ num = 1
 #            row.values_at("pid", "usename", "query")
 #   end
 # end
+
+# p CSV.read("Book2.csv")
+
+table = CSV.parse(File.read("Book2.csv"), headers: true)
+
+# table.each do |row|
+#   puts row
+#   sleep(1)
+# end
+
+# p table[0]["ID"].to_i
+
+headers = ["ID", "Mandate Id", "Accounting Id", "Match Field", "Paid On", "Paid", "Updated"]
+CSV.open("../../Documents/TurnaroundImport_8-7_Test_Write.csv", "w") do |csv|
+  # csv << headers
+  CSV.foreach("../../Documents/TurnaroundImport_8-7_Test.csv", headers: true, header_converters: :symbol, return_headers: true) do |row|
+    p row
+    csv << row
+  end
+end
